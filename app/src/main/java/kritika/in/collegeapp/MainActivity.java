c3e83179f9b44e048f09c44d1721f2ccf5937ee4
KritikaJain <jainkkritika.km@gmail.com>
2017-06-21 13:21:03 +0530

    added textView

--- app/src/main/java/kritika/in/collegeapp/MainActivity.java
+++ app/src/main/java/kritika/in/collegeapp/MainActivity.java
@@ -2,6 +2,7 @@ package kritika.in.collegeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView titleText= (TextView) findViewById(R.id.txt_titleText);
+        titleText.setText("something");
+
+
     }
 }
